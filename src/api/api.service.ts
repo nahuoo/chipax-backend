f27ee85e891fc0e061eddae7c19f5abfc08d2b54
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-01 16:36:12 -0300

    chore: refactor

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,21 +1,22 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { Observable, lastValueFrom, map } from 'rxjs';
-import axios, { AxiosResponse } from 'axios';
-import { response } from 'express';
+import { AxiosResponse } from 'axios';
 import { getArrayOfLocationsId } from '../utils/getArrayOfLocationsId';
 
 @Injectable()
 export class ApiService {
   constructor(private readonly httpService: HttpService) {}
 
-  async getCharCounter(): Promise<any> {
+  async getCharCounter(): Promise<number> {
     const arrayOfLocationsId = await getArrayOfLocationsId();
-    const allLocationsData = this.httpService
+    const allLocationsData: Observable<string[]> = this.httpService
       .get(`https://rickandmortyapi.com/api/location/${arrayOfLocationsId}`)
       .pipe(map((response: AxiosResponse) => response.data));
-    const unwrapLocationData = await lastValueFrom(allLocationsData);
-    const locationNames = unwrapLocationData.map((location) => location.name);
+    const unwrapLocationData: any[] = await lastValueFrom(allLocationsData);
+    const locationNames: string[] = unwrapLocationData.map(
+      (location) => location.name,
+    );
     const charLCounter = (
       locationNames.join('').toLowerCase().match(/l/g) || []
     ).length;
--- src/utils/getArrayOfLocationsId.ts
+++ src/utils/getArrayOfLocationsId.ts
@@ -1,12 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
 
 export const getArrayOfLocationsId = async (): Promise<number[]> => {
-  const response: AxiosResponse = await axios.get('https://rickandmortyapi.com/api/location/');
+  const response: AxiosResponse = await axios.get(
+    'https://rickandmortyapi.com/api/location/',
+  );
   const locationsCount = response.data.info.count;
   //create array from 0 to locationsCount
   const endpointOfLocationsIds: number[] = Array.from(
     { length: locationsCount },
-    (_, i) => i+1,
+    (_, i) => i + 1,
   );
   return endpointOfLocationsIds;
 };
