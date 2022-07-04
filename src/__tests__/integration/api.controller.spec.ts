8d64191ed3d070a8c854a2cc9686c65ee45bf7bc
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-04 16:13:18 -0300

    feat: api.controller test

--- src/__tests__/integration/api.controller.spec.ts
+++ src/__tests__/integration/api.controller.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ApiController } from './api.controller';
+import { ApiController } from '../../api/api.controller';
+import { ApiService } from '../../api/api.service';
+import { HttpModule } from '@nestjs/axios'
 
 describe('ApiController', () => {
   let controller: ApiController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [ApiController],
-    }).compile();
+      providers: [ApiService],
+    }).compile()
 
-    controller = module.get<ApiController>(ApiController);
+    controller = module.get(ApiController);
   });
 
   it('should be defined', () => {
