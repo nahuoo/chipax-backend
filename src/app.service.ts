fdc71a674aee10963008a45da31b0188ea4f5962
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-04 16:24:32 -0300

    chore: change the return object

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(){
+    return {
+      char_counter: '/api/char-counter',
+      episode_locations: '/api/episodes-locations',
+    }
   }
 }
