f9f70d1cee44aaf0f707bbd9b0a30210c4f0e4bf
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-04 16:18:36 -0300

    feat: root screen test

--- src/__tests__/integration/app.controller.spec.ts
+++ src/__tests__/integration/app.controller.spec.ts
@@ -15,8 +15,11 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return the api endpoints', () => {
+      expect(appController.getHello()).toBe({
+        char_counter: '/api/char-counter',
+        episode_locations: '/api/episodes-locations',
+      })
     });
   });
 });
