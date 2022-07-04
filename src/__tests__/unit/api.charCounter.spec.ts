b5b218821abd3126725ee08fb2a91b90a22bb4d8
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-04 15:54:09 -0300

    feat: charCounter test

--- src/__tests__/unit/api.charCounter.spec.ts
+++ src/__tests__/unit/api.charCounter.spec.ts
@@ -1,20 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { ApiService } from '../../src/api/api.service'
+import { ApiService } from '../../api/api.service'
+import { HttpModule } from '@nestjs/axios'
 
-describe('ApiService method charCounter', async () => {
-  let service: ApiService
-  const charCounter: any = await service.getCharCounter()
+describe('ApiService method charCounter', () => {
+  let service
+  let charCounter
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [ApiService],
     }).compile()
-    service = module.get<ApiService>(ApiService)
+    service = module.get(ApiService)
+    charCounter = await service.getCharCounter()
   })
 
   it('should be defined and return 82, 88 and 494', () => {
     expect(service).toBeDefined()
-    expect(charCounter).not.toBe(undefined)
+    expect(service.getCharCounter()).not.toBe(undefined)
     expect(charCounter.results[0].count).toEqual(82)
     expect(charCounter.results[1].count).toEqual(88)
     expect(charCounter.results[2].count).toEqual(494)
