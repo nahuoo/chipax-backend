ac02912505a5e1461a93bb68ba4cf686a6d0b897
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-04 15:56:49 -0300

    feat: episodeLocations test

--- src/__tests__/unit/api.episodesLocation.spec.ts
+++ src/__tests__/unit/api.episodesLocation.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { ApiService } from '../../src/api/api.service'
+import { ApiService } from '../../api/api.service'
+import { HttpModule } from '@nestjs/axios'
 
-describe('ApiService method getEpisodesLocaation', async () => {
-  let service: ApiService
-  const episodesLocations: any = await service.getEpisodesLocations()
-
+describe('ApiService method getEpisodesLocaation', () => {
+  let service
+  let episodesLocations
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [ApiService],
     }).compile()
     service = module.get<ApiService>(ApiService)
+    episodesLocations = await service.getEpisodesLocations()
   })
 
   it('should be defined and return all 51 episodes', () => {
